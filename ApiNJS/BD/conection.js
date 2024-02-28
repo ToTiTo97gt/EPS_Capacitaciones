5c8abda442a659adf41387f73bca21acedefde3f
Diego Vasquez <2992658970101@ingenieria.usac.edu.gt>
2024-02-28 12:29:10 -0600

    Actualizacion de base de datos

--- ApiNJS/BD/conection.js
+++ ApiNJS/BD/conection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql2');
 var connection = mysql.createConnection({
-    host: 'db', //db
-    user: 'root', //root
-    password: 'Rubik-60', //Rubik-60
-    database: 'jp_eps_conf', //jp_eps_conf
+    host: 'db-epost-fcjys-solicitudes.c4nomdu94wi7.us-east-1.rds.amazonaws.com', //db-epost-fcjys-solicitudes.c4nomdu94wi7.us-east-1.rds.amazonaws.com
+    user: 'Diego_Vasquez', //root
+    password: 'ToTiTo97gt', //Rubik-60
+    database: 'db_conferencia', //jp_eps_conf
     port: 3306
 });
 connection.connect(function(error){
